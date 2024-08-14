ff61fcca12f8ea323cc4d449de363ed902a04fff
socsasa15 <149267987+socsasa15@users.noreply.github.com>
2024-08-14 17:00:27 +0300

    Add files via upload

---
+++ alrowad_crm/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Unique Mobile and Email in Contacts",
+    "version": "15.0.1.0",
+    "author": "Silver Touch Technologies Limited",
+    "summary": "This module provides functionality of making email and mobile field unique in contact creation",
+    'category': 'contacts',
+    "website": "https://www.silvertouch.com/",
+    "description": """
+        """,
+    'depends': ['contacts'],
+    'data': [
+        'views/res_config.xml',
+    ],
+    "price": 0,
+    "currency": "USD",
+    "license": "LGPL-3",
+    'installable': True,
+    'application': False,
+    'images': ['static/description/banner.png']
+}
